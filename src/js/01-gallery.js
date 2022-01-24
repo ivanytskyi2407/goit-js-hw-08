fb42765b8e910b311e6014ebe4ea32f2a5ef7347
ivanytskyi2407 <89008107+ivanytskyi2407@users.noreply.github.com>
2022-01-24 23:43:28 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,15 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
-
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+const markup = galleryItems.map(({ preview, original, description }) => {
+       return `
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+}).join('');
+const galleryRef = document.querySelector('.gallery');
+galleryRef.insertAdjacentHTML('beforeend',markup);
+let gallery = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay:250,
+});
