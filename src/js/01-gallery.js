cf6831d0329ff8c2c1b30a43d5aa21f76089b621
ivanytskyi2407 <89008107+ivanytskyi2407@users.noreply.github.com>
2022-01-26 20:38:37 +0200

    123

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,15 +1,17 @@
 import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-const markup = galleryItems.map(({ preview, original, description }) => {
-       return `
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
 <a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>`
-}).join('');
+</a>`;
+  })
+  .join('');
 const galleryRef = document.querySelector('.gallery');
-galleryRef.insertAdjacentHTML('beforeend',markup);
+galleryRef.insertAdjacentHTML('beforeend', markup);
 let gallery = new SimpleLightbox('.gallery a', {
-    captionsData: "alt",
-    captionDelay:250,
+  captionsData: 'alt',
+  captionDelay: 250,
 });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,38 +1,37 @@
 import throttle from 'lodash.throttle';
 const refs = {
-    form: document.querySelector('.feedback-form'),
-    inputEmail: document.querySelector('input'),
-    textarea: document.querySelector('textarea'),
+  form: document.querySelector('.feedback-form'),
+  inputEmail: document.querySelector('input'),
+  textarea: document.querySelector('textarea'),
 };
-const STORAGE_KEY ='feedback-form-state';
+const STORAGE_KEY = 'feedback-form-state';
 const formData = {};
 refs.form.addEventListener('submit', onFormSubmit);
-refs.inputEmail.addEventListener('input', throttle(onInputEmail,500));
-refs.textarea.addEventListener('input', throttle(onInputTextarea,500));
+refs.inputEmail.addEventListener('input', throttle(onInputEmail, 500));
+refs.textarea.addEventListener('input', throttle(onInputTextarea, 500));
 function onFormSubmit(e) {
-    e.preventDefault();
-    console.log('відправили форму');
-    e.currentTarget.reset();  
-    localStorage.removeItem(STORAGE_KEY);
-    console.log(formData);
+  e.preventDefault();
+  console.log('відправили форму');
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  console.log(formData);
 }
 function onInputEmail(e) {
-    formData[e.target.name] = e.target.value;
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));  
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 function onInputTextarea(e) {
-    formData[e.target.name] = e.target.value;
-        localStorage.setItem(STORAGE_KEY,JSON.stringify(formData));  
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 function populateTextarea() {
-    const saveMessage = localStorage.getItem(STORAGE_KEY);
-    const parseMessage = JSON.parse(saveMessage);
-    if (parseMessage.message) {
-        refs.textarea.value = parseMessage.message;
-    }
-    if (parseMessage.email) {
-        refs.inputEmail.value = parseMessage.email;        
-    }
+  const saveMessage = localStorage.getItem(STORAGE_KEY);
+  const parseMessage = JSON.parse(saveMessage);
+  if (parseMessage.message) {
+    refs.textarea.value = parseMessage.message;
+  }
+  if (parseMessage.email) {
+    refs.inputEmail.value = parseMessage.email;
+  }
 }
 populateTextarea();
-
